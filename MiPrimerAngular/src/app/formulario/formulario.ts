7388315d679a58a0234203f8a8dbc5055afb297d
Belman Marin Franco <54726383+Scr1ptechnick@users.noreply.github.com>
2026-08-29 18:11:04 +0000

    CAMBIOS

---
+++ MiPrimerAngular/src/app/formulario/formulario.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-formulario',
+  styleUrl: './formulario.css',
+  templateUrl: './formulario.html',
+})
+export class Formulario {
+}
